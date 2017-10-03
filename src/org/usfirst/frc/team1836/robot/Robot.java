928d38cf540d7ae138f6307f8f34bc3458162fc3
Alexander Swerdlow <aswerdlow935@milkenschool.org>
2017-10-03 16:41:03 -0700

    Format

--- src/org/usfirst/frc/team1836/robot/Robot.java
+++ src/org/usfirst/frc/team1836/robot/Robot.java
@@ -47,4 +47,3 @@ public class Robot extends IterativeRobot {
   }
 
 }
-
--- src/org/usfirst/frc/team1836/robot/auto/commands/MagicMotionDrive.java
+++ src/org/usfirst/frc/team1836/robot/auto/commands/MagicMotionDrive.java
@@ -15,27 +15,32 @@ public class MagicMotionDrive extends Command {
   }
 
   // Called just before this Command runs the first time
+  @Override
   protected void initialize() {
     Drive.getInstance().setMotionMagicRelPosition(pos);
   }
 
   // Called repeatedly when this Command is scheduled to run
+  @Override
   protected void execute() {
 
   }
 
   // Make this return true when this Command no longer needs to run execute()
+  @Override
   protected boolean isFinished() {
     return Drive.getInstance().getMotionMagicOnTarget();
   }
 
   // Called once after isFinished returns true
+  @Override
   protected void end() {
 
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
+  @Override
   protected void interrupted() {
 
   }
