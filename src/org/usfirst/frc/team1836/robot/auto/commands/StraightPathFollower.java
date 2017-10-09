5ae9c302dbfbcc390f25afdd23883bb762f55373
Alexander Swerdlow <aswerdlow1@gmail.com>
2017-10-09 11:25:18 -0700

    Added Style Guide

--- src/org/usfirst/frc/team1836/robot/auto/commands/StraightPathFollower.java
+++ src/org/usfirst/frc/team1836/robot/auto/commands/StraightPathFollower.java
@@ -11,40 +11,42 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class StraightPathFollower extends Command {
 
-    private int cycle;
-    public StraightPathFollower() {
-      
-    }
+	private int cycle;
 
-    // Called just before this Command runs the first time
-    @Override
-    protected void initialize() {
-      cycle = 0;
-    }
+	public StraightPathFollower() {
 
-    // Called repeatedly when this Command is scheduled to run
-    @Override
-    protected void execute() {
-    	if(cycle < MotionProfile.kNumPoints) {
-      Drive.getInstance().setDriveTrajectory(new TrajectoryPoint(MotionProfile.Points[cycle][0], MotionProfile.Points[cycle][0], cycle+1, MotionProfile.Points[cycle][2]));
-      cycle++;
-    	}
-    }
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    @Override
-    protected boolean isFinished() {
-     	return cycle >= MotionProfile.kNumPoints;
-    }
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		cycle = 0;
+	}
 
-    // Called once after isFinished returns true
-    @Override
-    protected void end() {
-    }
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		if (cycle < MotionProfile.kNumPoints) {
+			Drive.getInstance().setDriveTrajectory(new TrajectoryPoint(MotionProfile.Points[cycle][0],
+					MotionProfile.Points[cycle][0], cycle + 1, MotionProfile.Points[cycle][2]));
+			cycle++;
+		}
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    @Override
-    protected void interrupted() {
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return cycle >= MotionProfile.kNumPoints;
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+	}
 }
