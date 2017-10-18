2785f67796605019da0a128f61a63c3637265580
Alexander Swerdlow <aswerdlow1@gmail.com>
2017-10-18 11:36:06 -0700

    nav

--- src/org/usfirst/frc/team1836/robot/Constants.java
+++ src/org/usfirst/frc/team1836/robot/Constants.java
@@ -2,55 +2,61 @@ package org.usfirst.frc.team1836.robot;
 
 public final class Constants {
 
-	public static class Hardware {
-		public static final int LEFT_FWD_TALON_ID = 1; 
-		public static final int LEFT_BACK_TALON_ID = 2;
-		public static final int RIGHT_FWD_TALON_ID = 3;
-		public static final int RIGHT_BACK_TALON_ID = 4;
-		public static final int GEAR_PICKUP_TALON_ID = 5;
-	}
+  public static class Hardware {
+    public static final int LEFT_FWD_TALON_ID = 1;
+    public static final int LEFT_BACK_TALON_ID = 2;
+    public static final int RIGHT_FWD_TALON_ID = 3;
+    public static final int RIGHT_BACK_TALON_ID = 4;
+    public static final int GEAR_PICKUP_TALON_ID = 5;
+  }
 
-	public static class Input {
-		public static final int DRIVE_STICK = 0;
-		public static final int OPERATOR_STICK = 1;
-		public static final int REVERSE_BUTTON = 1;
-		public static final int GEAR_PICKUP_BUTTON = 2;
-		public static final int GEAR_STOW_BUTTON = 3;
-		public static final int GEAR_PLACE_BUTTON = 4;
+  public static class Input {
+    public static final int DRIVE_STICK = 0;
+    public static final int OPERATOR_STICK = 1;
+    public static final int REVERSE_BUTTON = 1;
+    public static final int GEAR_PICKUP_BUTTON = 2;
+    public static final int GEAR_STOW_BUTTON = 3;
+    public static final int GEAR_PLACE_BUTTON = 4;
 
-	}
+  }
 
-	public static class PID {
-		public static final double GearA = 0.75 * 4400;
-		public static final double GearV = 0.75 * 4400;
-		public static final int GearIZone = 400;
-		public static final double GearP = (((0.1 * 1023) / 8000) * 16);
-		public static final double GearI = 0.001;
-		public static final double GearD = ((0.1 * 1023) / 8000) * 160;
-		public static final double GearF = (1.0 * 1023) / (4400 / 60 / 10 * 4096);
+  public static class PID {
+    public static final double GearA = 0.75 * 4400;
+    public static final double GearV = 0.75 * 4400;
+    public static final int GearIZone = 400;
+    public static final double GearP = (((0.1 * 1023) / 8000) * 16);
+    public static final double GearI = 0.001;
+    public static final double GearD = ((0.1 * 1023) / 8000) * 160;
+    public static final double GearF = (1.0 * 1023) / (4400 / 60 / 10 * 4096);
 
-		public static final double DriveA = 0;
-		public static final double DriveV = 0;
-		public static final int DriveIZone = 0;
-		public static final double DriveP = 0;
-		public static final double DriveI = 0;
-		public static final double DriveD = 0;
-		public static final double DriveF = 0;
-		public static final double DriveFollowerP = 0;
-		
-		public static final int codesPerRev = 1023;
-	}
+    public static final double DriveA = 0;
+    public static final double DriveV = 0;
+    public static final int DriveIZone = 0;
+    public static final double DriveP = 0;
+    public static final double DriveI = 0;
+    public static final double DriveD = 0;
+    public static final double DriveF = 0;
+    public static final double DriveFollowerP = 0;
 
-	public static class GearMech {
-		// From 0 to 10
-		public static final double GearPickup = 10;
-		public static final double GearStow = 5;
-		public static final double GearPlace = 0;
-	}
+    public static final int codesPerRev = 1023;
 
-	public static class Drive {
-		public static final double DriveMagicMotionTol = 1;
-		public static final double WheelDiameter = 6;
-	}
+    public static final double TurnP = 0;
+    public static final double TurnI = 0;
+    public static final double TurnD = 0;
+    public static final double TurnF = 0;
+    public static final double TurnTol = 0;
+  }
+
+  public static class GearMech {
+    // From 0 to 10
+    public static final double GearPickup = 10;
+    public static final double GearStow = 5;
+    public static final double GearPlace = 0;
+  }
+
+  public static class Drive {
+    public static final double DriveMagicMotionTol = 1;
+    public static final double WheelDiameter = 6;
+  }
 
 }
--- src/org/usfirst/frc/team1836/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team1836/robot/subsystems/Drive.java
@@ -8,146 +8,166 @@ import org.usfirst.frc.team1836.robot.util.TrajectoryPoint;
 
 import com.ctre.CANTalon.FeedbackDevice;
 import com.ctre.CANTalon.TalonControlMode;
+import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Drive extends Subsystem {
 
-	private static Drive drive;
-	private final RobotDrive robotDr;
-	final MkCANTalon leftfwdtalon = new MkCANTalon(Constants.Hardware.LEFT_FWD_TALON_ID, Constants.Drive.WheelDiameter,
-			false);
-	final MkCANTalon leftbacktalon = new MkCANTalon(Constants.Hardware.LEFT_BACK_TALON_ID,
-			Constants.Drive.WheelDiameter, false);
-	final MkCANTalon rightfwdtalon = new MkCANTalon(Constants.Hardware.RIGHT_FWD_TALON_ID,
-			Constants.Drive.WheelDiameter, false);
-	final MkCANTalon rightbacktalon = new MkCANTalon(Constants.Hardware.RIGHT_BACK_TALON_ID,
-			Constants.Drive.WheelDiameter, false);
-	private TrajectoryPoint lastPoint;
+  private static Drive drive;
+  private final RobotDrive robotDr;
+  final MkCANTalon leftfwdtalon;
+  final MkCANTalon leftbacktalon;
+  final MkCANTalon rightfwdtalon;
+  final MkCANTalon rightbacktalon;
+  AHRS navX;
+  PIDController turnController;
 
-	public static Drive getInstance() {
-		if (drive == null)
-			drive = new Drive();
-		return drive;
-	}
+  private TrajectoryPoint lastPoint;
 
-	public Drive() {
-		super("Drive");
-		robotDr = new RobotDrive(leftfwdtalon, leftbacktalon, rightfwdtalon, rightbacktalon);
+  public static Drive getInstance() {
+    if (drive == null)
+      drive = new Drive();
+    return drive;
+  }
 
-		leftfwdtalon.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
-		leftfwdtalon.reverseSensor(false);
-		leftfwdtalon.configNominalOutputVoltage(+0.0f, -0.0f);
-		leftfwdtalon.configPeakOutputVoltage(+12.0f, -12.0f);
-		leftfwdtalon.setProfile(0);
-		leftfwdtalon.setF(Constants.PID.DriveF);
-		leftfwdtalon.setP(Constants.PID.DriveP);
-		leftfwdtalon.setI(Constants.PID.DriveI);
-		leftfwdtalon.setD(Constants.PID.DriveD);
-		leftfwdtalon.setIZone(Constants.PID.DriveIZone);
-		leftfwdtalon.setMotionMagicCruiseVelocity(Constants.PID.DriveV);
-		leftfwdtalon.setMotionMagicAcceleration(Constants.PID.DriveA);
+  public Drive() {
+    super("Drive");
+    navX = new AHRS(SPI.Port.kMXP);
+    leftfwdtalon =
+        new MkCANTalon(Constants.Hardware.LEFT_FWD_TALON_ID, Constants.Drive.WheelDiameter, false);
+    leftbacktalon =
+        new MkCANTalon(Constants.Hardware.LEFT_BACK_TALON_ID, Constants.Drive.WheelDiameter, false);
+    rightbacktalon = new MkCANTalon(Constants.Hardware.RIGHT_BACK_TALON_ID,
+        Constants.Drive.WheelDiameter, false);
+    rightfwdtalon =
+        new MkCANTalon(Constants.Hardware.RIGHT_FWD_TALON_ID, Constants.Drive.WheelDiameter, false);
 
-		rightfwdtalon.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
-		rightfwdtalon.reverseSensor(false);
-		rightfwdtalon.configNominalOutputVoltage(+0.0f, -0.0f);
-		rightfwdtalon.configPeakOutputVoltage(+12.0f, -12.0f);
-		rightfwdtalon.setProfile(0);
-		rightfwdtalon.setF(Constants.PID.DriveF);
-		rightfwdtalon.setP(Constants.PID.DriveP);
-		rightfwdtalon.setI(Constants.PID.DriveI);
-		rightfwdtalon.setD(Constants.PID.DriveD);
-		rightfwdtalon.setIZone(Constants.PID.DriveIZone);
-		rightfwdtalon.setMotionMagicCruiseVelocity(Constants.PID.DriveV);
-		rightfwdtalon.setMotionMagicAcceleration(Constants.PID.DriveA);
+    robotDr = new RobotDrive(leftfwdtalon, leftbacktalon, rightfwdtalon, rightbacktalon);
 
-		leftfwdtalon.setPrint(false);
-		leftbacktalon.setPrint(false);
-		rightfwdtalon.setPrint(false);
-		rightbacktalon.setPrint(false);
-	}
+    leftfwdtalon.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
+    leftfwdtalon.reverseSensor(false);
+    leftfwdtalon.configNominalOutputVoltage(+0.0f, -0.0f);
+    leftfwdtalon.configPeakOutputVoltage(+12.0f, -12.0f);
+    leftfwdtalon.setProfile(0);
+    leftfwdtalon.setF(Constants.PID.DriveF);
+    leftfwdtalon.setP(Constants.PID.DriveP);
+    leftfwdtalon.setI(Constants.PID.DriveI);
+    leftfwdtalon.setD(Constants.PID.DriveD);
+    leftfwdtalon.setIZone(Constants.PID.DriveIZone);
+    leftfwdtalon.setMotionMagicCruiseVelocity(Constants.PID.DriveV);
+    leftfwdtalon.setMotionMagicAcceleration(Constants.PID.DriveA);
 
-	@Override
-	public void initTeleop() {
-		leftfwdtalon.changeControlMode(TalonControlMode.PercentVbus);
-		leftbacktalon.changeControlMode(TalonControlMode.PercentVbus);
-		rightfwdtalon.changeControlMode(TalonControlMode.PercentVbus);
-		rightbacktalon.changeControlMode(TalonControlMode.PercentVbus);
-		leftfwdtalon.setEncPosition(0);
-		rightfwdtalon.setEncPosition(0);
-	}
+    rightfwdtalon.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
+    rightfwdtalon.reverseSensor(false);
+    rightfwdtalon.configNominalOutputVoltage(+0.0f, -0.0f);
+    rightfwdtalon.configPeakOutputVoltage(+12.0f, -12.0f);
+    rightfwdtalon.setProfile(0);
+    rightfwdtalon.setF(Constants.PID.DriveF);
+    rightfwdtalon.setP(Constants.PID.DriveP);
+    rightfwdtalon.setI(Constants.PID.DriveI);
+    rightfwdtalon.setD(Constants.PID.DriveD);
+    rightfwdtalon.setIZone(Constants.PID.DriveIZone);
+    rightfwdtalon.setMotionMagicCruiseVelocity(Constants.PID.DriveV);
+    rightfwdtalon.setMotionMagicAcceleration(Constants.PID.DriveA);
 
-	@Override
-	public void updateTeleop() {
-		robotDr.arcadeDrive(Inputs.driverJoystick.getRawAxis(1), Inputs.driverJoystick.getRawAxis(2));
-	}
+    leftfwdtalon.setPrint(false);
+    leftbacktalon.setPrint(false);
+    rightfwdtalon.setPrint(false);
+    rightbacktalon.setPrint(false);
 
-	@Override
-	public void initAuto() {
-		leftfwdtalon.changeControlMode(TalonControlMode.MotionMagic);
-		leftbacktalon.changeControlMode(TalonControlMode.Follower);
-		leftbacktalon.set(Constants.Hardware.LEFT_FWD_TALON_ID);
-		rightfwdtalon.changeControlMode(TalonControlMode.MotionMagic);
-		rightbacktalon.changeControlMode(TalonControlMode.Follower);
-		rightbacktalon.set(Constants.Hardware.RIGHT_BACK_TALON_ID);
-		leftfwdtalon.setEncPosition(0);
-		rightfwdtalon.setEncPosition(0);
-	}
+    turnController = new PIDController(Constants.PID.TurnP, Constants.PID.TurnI, Constants.PID.TurnD, Constants.PID.TurnF, navX, output -> {
+      robotDr.tankDrive(output, -output);
+    });
+    turnController.setInputRange(-180.0f, 180.0f);
+    turnController.setOutputRange(-1.0, 1.0);
+    turnController.setAbsoluteTolerance(Constants.PID.TurnTol);
+    turnController.setContinuous(true);
+  }
 
-	@Override
-	public void updateAuto() {
+  @Override
+  public void initTeleop() {
+    leftfwdtalon.changeControlMode(TalonControlMode.PercentVbus);
+    leftbacktalon.changeControlMode(TalonControlMode.PercentVbus);
+    rightfwdtalon.changeControlMode(TalonControlMode.PercentVbus);
+    rightbacktalon.changeControlMode(TalonControlMode.PercentVbus);
+    leftfwdtalon.setEncPosition(0);
+    rightfwdtalon.setEncPosition(0);
+  }
 
-	}
+  @Override
+  public void updateTeleop() {
+    robotDr.arcadeDrive(Inputs.driverJoystick.getRawAxis(1), Inputs.driverJoystick.getRawAxis(2));
+  }
 
-	@Override
-	public void sendToSmartDash() {
-		SmartDashboard.putNumber("Left Encoder Position", leftfwdtalon.getEncPosition());
-		SmartDashboard.putNumber("Right Encoder Position", rightfwdtalon.getEncPosition());
-		SmartDashboard.putNumber("Left Encoder Velocity", leftfwdtalon.getEncVelocity());
-		SmartDashboard.putNumber("Right Encoder Velocity", rightfwdtalon.getEncVelocity());
-	}
+  @Override
+  public void initAuto() {
+    leftfwdtalon.changeControlMode(TalonControlMode.MotionMagic);
+    leftbacktalon.changeControlMode(TalonControlMode.Follower);
+    leftbacktalon.set(Constants.Hardware.LEFT_FWD_TALON_ID);
+    rightfwdtalon.changeControlMode(TalonControlMode.MotionMagic);
+    rightbacktalon.changeControlMode(TalonControlMode.Follower);
+    rightbacktalon.set(Constants.Hardware.RIGHT_BACK_TALON_ID);
+    leftfwdtalon.setEncPosition(0);
+    rightfwdtalon.setEncPosition(0);
+  }
 
-	public void setMotionMagicRelPosition(double pos) {
-		leftfwdtalon.setEncPosition(0);
-		rightfwdtalon.setEncPosition(0);
-		leftfwdtalon.set(pos);
-		rightfwdtalon.set(pos);
-	}
+  @Override
+  public void updateAuto() {
 
-	public void setMotionMagicAbsPosition(double pos) {
-		leftfwdtalon.set(pos);
-		rightfwdtalon.set(pos);
-	}
+  }
 
-	public boolean getMotionMagicOnTarget() {
-		if (Math.abs(leftfwdtalon.getClosedLoopError()) <= Constants.Drive.DriveMagicMotionTol
-				&& Math.abs(rightfwdtalon.getClosedLoopError()) <= Constants.Drive.DriveMagicMotionTol) {
-			return true;
-		} else {
-			return false;
-		}
+  @Override
+  public void sendToSmartDash() {
+    SmartDashboard.putNumber("Left Encoder Position", leftfwdtalon.getEncPosition());
+    SmartDashboard.putNumber("Right Encoder Position", rightfwdtalon.getEncPosition());
+    SmartDashboard.putNumber("Left Encoder Velocity", leftfwdtalon.getEncVelocity());
+    SmartDashboard.putNumber("Right Encoder Velocity", rightfwdtalon.getEncVelocity());
+  }
 
-	}
+  public void setMotionMagicRelPosition(double pos) {
+    leftfwdtalon.setEncPosition(0);
+    rightfwdtalon.setEncPosition(0);
+    leftfwdtalon.set(pos);
+    rightfwdtalon.set(pos);
+  }
 
-	public void setDriveVelocity(double velocity) {
-		leftfwdtalon.changeControlMode(TalonControlMode.Speed);
-		rightfwdtalon.changeControlMode(TalonControlMode.Speed);
-		leftfwdtalon.set(velocity);
-		rightfwdtalon.set(velocity);
-	}
+  public void setMotionMagicAbsPosition(double pos) {
+    leftfwdtalon.set(pos);
+    rightfwdtalon.set(pos);
+  }
 
-	public void setDriveTrajectory(TrajectoryPoint point) {
-		if (point.getCount() != 1) {
-			double leftVel = point.getVel()
-					+ ((lastPoint.getPos() - leftfwdtalon.getPosition()) * Constants.PID.DriveFollowerP);
-			double rightVel = point.getVel()
-					+ ((lastPoint.getPos() - rightfwdtalon.getPosition()) * Constants.PID.DriveFollowerP);
-			leftfwdtalon.set(leftVel);
-			rightfwdtalon.set(rightVel);
-		}
-		System.out.println(point);
-		lastPoint = point;
-	}
+  public boolean getMotionMagicOnTarget() {
+    if (Math.abs(leftfwdtalon.getClosedLoopError()) <= Constants.Drive.DriveMagicMotionTol
+        && Math.abs(rightfwdtalon.getClosedLoopError()) <= Constants.Drive.DriveMagicMotionTol) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+  public void setDriveVelocity(double velocity) {
+    leftfwdtalon.changeControlMode(TalonControlMode.Speed);
+    rightfwdtalon.changeControlMode(TalonControlMode.Speed);
+    leftfwdtalon.set(velocity);
+    rightfwdtalon.set(velocity);
+  }
+
+  public void setDriveTrajectory(TrajectoryPoint point) {
+    if (point.getCount() != 1) {
+      double leftVel = point.getVel()
+          + ((lastPoint.getPos() - leftfwdtalon.getPosition()) * Constants.PID.DriveFollowerP);
+      double rightVel = point.getVel()
+          + ((lastPoint.getPos() - rightfwdtalon.getPosition()) * Constants.PID.DriveFollowerP);
+      leftfwdtalon.set(leftVel);
+      rightfwdtalon.set(rightVel);
+    }
+    System.out.println(point);
+    lastPoint = point;
+  }
 
 }
